4e21a5591ce43be81996741b742acb7abde1a955
jvdarkar88-collab <jvdarkar88@gmail.com>
2026-02-21 08:28:11 -0600

    Estado Inicial

---
+++ src/main/java/com/educacion/recursos/RecursosApplication.java
@@ -0,0 +1,13 @@
+package com.educacion.recursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecursosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educacion/recursos/RecursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educacion.recursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
